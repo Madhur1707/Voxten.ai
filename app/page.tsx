e8612cf1adf23389f6a5068b916c0a0703ff7f62
Madhur Pathak <117804227+Madhur1707@users.noreply.github.com>
2025-04-26 16:31:13 +0530

    Project Setup

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      Hello World!
+      <Button>Click Me</Button>
+    </div>
+  )
+}
+
+export default Page
